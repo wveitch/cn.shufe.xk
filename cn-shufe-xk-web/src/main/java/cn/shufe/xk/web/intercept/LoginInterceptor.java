0c803e3e3433b1a53f1a5f77bbac466af71a9a80
veitch <veitch.wang@huolala.cn>
2023-11-20 10:56:53 +0800

    登录拦截器1

--- cn-shufe-xk-web/src/main/java/cn/shufe/xk/web/intercept/LoginInterceptor.java
+++ cn-shufe-xk-web/src/main/java/cn/shufe/xk/web/intercept/LoginInterceptor.java
@@ -1,5 +1,8 @@
 package cn.shufe.xk.web.intercept;
 
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.asymmetric.Sign;
+import cn.hutool.crypto.asymmetric.SignAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,11 +15,19 @@ public class LoginInterceptor implements HandlerInterceptor {
     private static final String SIGN_HEADER = "sign";
 
     // 获取token 签名
+    // 签名可以用hutool中的sign
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
         // https://juejin.cn/post/6844903758946664462
         String sign = request.getHeader(SIGN_HEADER);
+        // hutool签名策略
+        byte[] data = "我是一段测试字符串".getBytes();
+        Sign sign1 = SecureUtil.sign(SignAlgorithm.MD5withRSA);
+        //签名
+        byte[] signed = sign1.sign(data);
+        //验证签名
+        boolean verify = sign1.verify(data, signed);
         log.info("输出日志信息,sign:{}", sign);
         return true;
     }
